b351337cb2d504551e05f9a3f78f1f53c41aa456
Emilio C Marques <emiliocaveari@gmail.com>
2023-08-03 21:00:59 -0300

    Cidade model factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Cidade;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Emílio C. Marques',
+            'email' => 'emilio@example.com',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $cidades = Cidade::factory()->count(11)
+            ->state(new Sequence(
+                    [ 'nome' => 'Santo Antônio de Pádua', 'estado' => 'Rio de Janeiro'],
+                    [ 'nome' => 'Aperibé', 'estado' => 'Rio de Janeiro'],
+                    [ 'nome' => 'Itaocara', 'estado' => 'Rio de Janeiro'],
+                    [ 'nome' => 'Miracema', 'estado' => 'Rio de Janeiro'],
+                    [ 'nome' => 'Palma', 'estado' => 'Minas Gerais'],
+                    [ 'nome' => 'Além Paraíba', 'estado' => 'Minas Gerais'],
+                    [ 'nome' => 'Juiz de Fora', 'estado' => 'Minas Gerais'],
+                    [ 'nome' => 'Vitória', 'estado' => 'Espirito Santo'],
+                    [ 'nome' => 'Guarapai', 'estado' => 'Espirito Santo'],
+                    [ 'nome' => 'Piúma', 'estado' => 'Espirito Santo'],
+                    [ 'nome' => 'Marataízes', 'estado' => 'Espirito Santo'],
+            ))
+            ->create();
     }
 }
